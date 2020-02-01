d3b5a6f880f62d16cc23470b14d5c8bb7c2d1e33
Kieran Brown <kieranjbrown1@gmail.com>
2020-02-01 11:41:30 +0000

    KB - added test for retrieving trades by ID in order of version

--- src/test/java/kieranbrown/bitemp/database/TradeRepositoryTest.java
+++ src/test/java/kieranbrown/bitemp/database/TradeRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -73,7 +74,7 @@ class TradeRepositoryTest {
         repository.save(trade1);
         repository.save(trade2);
         System.out.println();
-        final Trade retrievedTrade = repository.findByTradeId(tradeId);
+        final Trade retrievedTrade = repository.findMostRecent(tradeId);
         assertThat(retrievedTrade).isNotNull()
                 .hasFieldOrPropertyWithValue("stock", "GOOGL")
                 .hasFieldOrPropertyWithValue("marketLimitFlag", 'M')
@@ -82,6 +83,63 @@ class TradeRepositoryTest {
                 .hasFieldOrPropertyWithValue("volume", 190)
                 .hasFieldOrPropertyWithValue("tradeKey", key2)
                 .hasFieldOrPropertyWithValue("systemTimeStart", new Date(2009, 10, 10, 10, 30, 0))
+                .hasFieldOrPropertyWithValue("systemTimeEnd", new Date(9999, 12, 31, 0, 0, 0))
+                .hasFieldOrPropertyWithValue("validTimeStart", new Date(2009, 10, 10, 29, 57, 0))
+                .hasFieldOrPropertyWithValue("validTimeEnd", new Date(9999, 12, 31, 0, 0, 0));
+    }
+
+    @Test
+    void canRetrieveAllByIdInAscOrder() {
+        final Trade trade1 = new Trade();
+        final Trade trade2 = new Trade();
+        final UUID tradeId = UUID.randomUUID();
+        final BitemporalKey key1 = new BitemporalKey.Builder().setTradeId(tradeId).setVersion(200).build();
+        final BitemporalKey key2 = new BitemporalKey.Builder().setTradeId(tradeId).setVersion(201).build();
+        trade1.setTradeKey(key1)
+                .setStock("GOOGL")
+                .setBuySellFlag('B')
+                .setMarketLimitFlag('M')
+                .setPrice(new BigDecimal("140.171"))
+                .setVolume(200)
+                .setSystemTimeStart(new Date(2009, 10, 10, 10, 0, 0))
+                .setSystemTimeEnd(new Date(2009, 10, 10, 30, 30, 0))
+                .setValidTimeStart(new Date(2009, 10, 10, 9, 30, 0))
+                .setValidTimeEnd(new Date(2009, 10, 10, 29, 57, 0));
+
+        trade2.setTradeKey(key2)
+                .setStock("GOOGL")
+                .setBuySellFlag('B')
+                .setMarketLimitFlag('M')
+                .setPrice(new BigDecimal("142.120"))
+                .setVolume(190)
+                .setSystemTimeStart(new Date(2009, 10, 10, 30, 30, 0))
+                .setValidTimeStart(new Date(2009, 10, 10, 29, 57, 0));
+
+        repository.save(trade1);
+        repository.save(trade2);
+        System.out.println();
+        final List<Trade> retrievedTrade = repository.findAllById(tradeId);
+
+        assertThat(retrievedTrade.get(0)).isNotNull()
+                .hasFieldOrPropertyWithValue("stock", "GOOGL")
+                .hasFieldOrPropertyWithValue("marketLimitFlag", 'M')
+                .hasFieldOrPropertyWithValue("buySellFlag", 'B')
+                .hasFieldOrPropertyWithValue("price", new BigDecimal("140.171"))
+                .hasFieldOrPropertyWithValue("volume", 200)
+                .hasFieldOrPropertyWithValue("tradeKey", key1)
+                .hasFieldOrPropertyWithValue("systemTimeStart", new Date(2009, 10, 10, 10, 0, 0))
+                .hasFieldOrPropertyWithValue("systemTimeEnd", new Date(2009, 10, 10, 30, 30, 0))
+                .hasFieldOrPropertyWithValue("validTimeStart", new Date(2009, 10, 10, 9, 30, 0))
+                .hasFieldOrPropertyWithValue("validTimeEnd", new Date(2009, 10, 10, 29, 57, 0));
+
+        assertThat(retrievedTrade.get(1)).isNotNull()
+                .hasFieldOrPropertyWithValue("stock", "GOOGL")
+                .hasFieldOrPropertyWithValue("marketLimitFlag", 'M')
+                .hasFieldOrPropertyWithValue("buySellFlag", 'B')
+                .hasFieldOrPropertyWithValue("price", new BigDecimal("142.120"))
+                .hasFieldOrPropertyWithValue("volume", 190)
+                .hasFieldOrPropertyWithValue("tradeKey", key2)
+                .hasFieldOrPropertyWithValue("systemTimeStart", new Date(2009, 10, 10, 30, 30, 0))
                 .hasFieldOrPropertyWithValue("systemTimeEnd", new Date(9999, 12, 31, 0, 0, 0))
                 .hasFieldOrPropertyWithValue("validTimeStart", new Date(2009, 10, 10, 29, 57, 0))
                 .hasFieldOrPropertyWithValue("validTimeEnd", new Date(9999, 12, 31, 0, 0, 0));
