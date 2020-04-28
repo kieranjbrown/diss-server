75084af0744bd414e47befee97ca138072bf3ac1
Kieran Brown <kieranjbrown1@gmail.com>
2020-04-28 11:56:46 +0100

    KB - added 3 distinct schemas to flyway for different types of testing

--- src/main/resources/db/migration/V1.0.0__schema.sql
+++ src/main/resources/db/migration/V1.0.0__schema.sql
@@ -1,21 +1,23 @@
-create schema if not exists reporting;
-
-create table if not exists reporting.trade_data
-(
-id UUID default random_uuid() not null,
-stock varchar(5) not null,
-price decimal(5,2) not null,
-volume int not null,
-valid_time_start date not null,
-valid_time_end date default '9999-12-31',
-system_time_start datetime(6),
-system_time_end datetime(6) default '9999-12-31 00:00:00.000000',
-buy_sell_flag char(1) not null,
-market_limit_flag char(1) not null
-);
-
-alter table reporting.trade_data alter column system_time_start set default CURRENT_TIMESTAMP;
+-- create schema if not exists reporting;
 --
+-- -- MY IMPLEMENTATION H2 SCHEMA
+-- -- create table if not exists reporting.trade_data
+-- -- (
+-- -- id UUID not null,
+-- -- stock varchar(5) not null,
+-- -- price decimal(5,2) not null,
+-- -- volume int not null,
+-- -- valid_time_start date not null,
+-- -- valid_time_end date not null,
+-- -- system_time_start timestamp(6),
+-- -- system_time_end timestamp(6),
+-- -- buy_sell_flag char(1) not null,
+-- -- market_limit_flag char(1) not null
+-- -- );
+-- --
+-- -- alter table reporting.trade_data alter column system_time_start set default CURRENT_TIMESTAMP;
+--
+-- -- MY IMPLEMENTATION MARIADB SCHEMA
 -- create table if not exists reporting.trade_data
 -- (
 -- id varchar(128) not null,
@@ -24,11 +26,27 @@ alter table reporting.trade_data alter column system_time_start set default CURR
 -- volume int not null,
 -- valid_time_start date not null,
 -- valid_time_end date not null,
--- system_time_start timestamp(6) AS ROW START,
--- system_time_end timestamp(6) AS ROW END,
+-- system_time_start timestamp(6),
+-- system_time_end timestamp(6),
 -- buy_sell_flag char(1) not null,
--- market_limit_flag char(1) not null,
--- PERIOD FOR application_time(valid_time_start, valid_time_end),
--- PERIOD FOR system_time(system_time_start, system_time_end)
--- )
--- WITH SYSTEM VERSIONING;
+-- market_limit_flag char(1) not null
+-- );
+-- alter table reporting.trade_data alter column system_time_start set default CURRENT_TIMESTAMP;
+-- --
+-- -- -- MARIADB IMPLEMENTATION SCHEMA
+create table if not exists reporting.trade_data
+(
+id varchar(128) primary key not null,
+stock varchar(5) not null,
+price decimal(5,2) not null,
+volume int not null,
+valid_time_start date not null,
+valid_time_end date not null,
+system_time_start timestamp(6) AS ROW START,
+system_time_end timestamp(6) AS ROW END,
+buy_sell_flag char(1) not null,
+market_limit_flag char(1) not null,
+PERIOD FOR application_time(valid_time_start, valid_time_end),
+PERIOD FOR system_time(system_time_start, system_time_end)
+)
+WITH SYSTEM VERSIONING;
